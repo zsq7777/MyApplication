ebff8b450880161e03f682efd8eb323a68370f6f
zhaosiqi <zsqandzyr@qq.com>
2021-04-13 18:01:57 +0800

    ui功能添加

--- CheckIn/src/main/java/com/able/checkin/MainActivity.kt
+++ CheckIn/src/main/java/com/able/checkin/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.able.checkin
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.OneTimeWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.able.checkin.workmanager.CheckInWorkManager
+import com.google.android.material.switchmaterial.SwitchMaterial
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
         //使用构建器
 //        val checkInWorkRequest = PeriodicWorkRequestBuilder<CheckInWorkManager>(2, TimeUnit.MINUTES)
 //            .build()
 
         // Additional configuration
-
         val checkInWorkRequest =
             PeriodicWorkRequestBuilder<CheckInWorkManager>(15, TimeUnit.MINUTES)
                 .build()
@@ -33,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         WorkManager.getInstance(this).enqueueUniquePeriodicWork("checkIn",
             ExistingPeriodicWorkPolicy.REPLACE,checkInWorkRequest)
 
+    }
+
+    private fun initView() {
+        //增加打卡时间规则
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+
+        }
+        //是否开启大小周  , 本周是大周还是小周。
+        findViewById<SwitchMaterial>(R.id.btn2).setOnClickListener {
+
+        }
+        //是否开启自动打卡
+        findViewById<SwitchMaterial>(R.id.btn3).setOnClickListener {
+
+        }
     }
 
 }
