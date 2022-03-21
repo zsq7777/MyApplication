602302203ac9d29656cb2d855550b3f1a7720076
zsqan <zsqandzyr@gmail.com>
2022-03-21 20:16:48 +0800

    修改打卡相关代码

--- CheckIn/src/main/java/com/able/checkin/datasource/CheckInEntity.kt
+++ CheckIn/src/main/java/com/able/checkin/datasource/CheckInEntity.kt
@@ -3,4 +3,9 @@ package com.able.checkin.datasource
 import java.time.Instant
 
 //Instant 时间戳 Duration 时间段
-data class CheckInEntity(var workTime: Instant)
+data class CheckInEntity(
+    var workTime: Instant,
+    var workRealTime: Instant?,
+    var tvGoOffWorkTime: Instant,
+    var tvGoOffWorkRealTime: Instant?
+)
