e8d66997461a4855a37f4bfc51c21adc32f6a93f
zhaosiqi <zsqandzyr@qq.com>
2022-03-21 09:08:59 +0800

    m

--- CheckIn/src/main/java/com/able/checkin/MainActivity.kt
+++ CheckIn/src/main/java/com/able/checkin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.os.PowerManager
 import android.provider.Settings
 import android.util.Log
